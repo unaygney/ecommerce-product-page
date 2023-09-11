854ad4b96e7d5e230ccd1afaf19a1199e995dad5
Guney Unay <guneymarketing17@hotmail.com>
2023-09-11 17:59:44 +0300

    header style is done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Items } from "./constants";
 //Icons
 import Logo from "../assets/logo.svg";
@@ -7,19 +7,23 @@ import Basket from "../assets/Basket.svg";
 import ProfilePic from "../assets/image-avatar.png";
 import Close from "../assets/Close.svg";
 
-
 function Header() {
-  const [isActive , setIsActive] = useState(false)
-  
+  const [isActive, setIsActive] = useState(false);
+  const [isCardActive, setIsCardActive] = useState(false);
+
+  console.log(isCardActive);
   return (
-    <header className="container mx-auto flex items-center md:h-[110px] h-[68px] border-b-2 border-slate-300">
+    <header className="container mx-auto flex items-center md:h-[110px] h-[68px] border-b-2 z-10 border-slate-300">
       <div className="flex md:hidden align-center m-4">
-        <button onClick={() => setIsActive(!isActive)} className="flex-shrink-0">
+        <button
+          onClick={() => setIsActive(!isActive)}
+          className="flex-shrink-0"
+        >
           <img src={HamburgerBtn} alt="Burger Button" />
         </button>
       </div>
 
-      <a href="#" className="mr-10 sm:mr-12 flex-shrink-0"> 
+      <a href="#" className="mr-10 sm:mr-12 flex-shrink-0">
         <img src={Logo} alt="logo" />
       </a>
 
@@ -28,20 +32,32 @@ function Header() {
         <ul className="md:flex gap-9 hidden  ">
           {Items.map((item) => (
             <li key={item.id}>
-              <a className="hover:border-b-4  border-orange-500 pb-11" href="#">{item.linkName}</a>
+              <a className="hover:border-b-4  border-orange-500 pb-11" href="#">
+                {item.linkName}
+              </a>
             </li>
           ))}
         </ul>
       </nav>
 
-
-      {/* Hamburger Menu for Mobile */}
       {/* Overlay */}
-      <div className={` fixed top-0 left-0 right-0 bottom-0 bg-black opacity-40 ${isActive ? "" : "hidden"}  ` } ></div>
-      <nav className={`bg-white flex flex-col gap-10 absolute p-5 top-0 left-0 bottom-0 z-50 w-[70%] ${isActive ? "" : "hidden"} `} >
-   
+      {/* Hamburger Menu for Mobile */}
+      <div
+        className={` fixed top-0 left-0 right-0 bottom-0 bg-black opacity-40 z-20 ${
+          isActive ? "" : "hidden"
+        }  `}
+      ></div>
+      <nav
+        className={`bg-white flex flex-col gap-10 absolute p-5 top-0 left-0 bottom-0 z-30 w-[70%] ${
+          isActive ? "" : "hidden"
+        } `}
+      >
         <button>
-          <img src={Close} onClick={() => setIsActive(!isActive)} alt="close button" />
+          <img
+            src={Close}
+            onClick={() => setIsActive(!isActive)}
+            alt="close button"
+          />
         </button>
         <ul className=" flex flex-col gap-5">
           {Items.map((item) => (
@@ -52,18 +68,33 @@ function Header() {
             </li>
           ))}
         </ul>
-  
       </nav>
       {/* basket */}
-      <div className="flex ml-auto sm:gap-11 gap-8 flex-shrink-0 md:-z-10">
-        <button className="cursor-pointer  relative ">
-          <div className=" bg-orange-500 absolute top-0 -right-3  items-center justify-center text-white rounded-[6.5px] text-xs font-bold py-1 px-1.5 hidden "></div>
+      <div className="flex ml-auto sm:gap-11 gap-8 flex-shrink-0  relative">
+        <button
+          className="cursor-pointer  "
+          onClick={() => setIsCardActive(!isCardActive)}
+        >
           <img src={Basket} alt="basket image " />
         </button>
+        {/* Item Card */}
+        {isCardActive && (
+          <div className="absolute right-1 top-20 w-[300px] sm:w-[360px] h-[256px] rounded-xl bg-white shadow-2xl flex flex-col ">
+            <div className="h-[67px] border-b-2 border-gray-200 p-6">
+              <h4 className="font-bold ">Card</h4>
+            </div>
 
-        <div className="w-[50px] h-[50px] cursor-pointer flex-shrink-0">
+            <div className="flex-1 flex items-center justify-center">
+              <p className="font-bold text-sm text-[#69707D] ">Your cart is empty.</p>
+            </div>
+
+
+          </div>
+        )}
+
+        <div className="w-[50px] h-[50px] cursor-pointer flex-shrink-0 hover:border-orange-500 rounded-full hover:border-2">
           <img
-            className="hover:border-orange-500 hover:border-2 rounded-full"
+            className=" rounded-full"
             src={ProfilePic}
             alt="profile picture"
           />
